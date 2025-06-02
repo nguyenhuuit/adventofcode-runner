ed9cd4a31470129aecc6f5a8c6854d73dfae5a4a
Huu Nguyen <nguyenhuuit@yahoo.com>
2025-06-02 23:32:42 +0700

    fix: javascript wrong path of solution file

--- src/drivers/javascript/javascript.js
+++ src/drivers/javascript/javascript.js
@@ -1,14 +1,13 @@
 import './global.js';
 import fs from 'fs';
 import path from 'path'
-import { EXTENSIONS } from '../../utils/languages.js'
 
 const year = process.argv[2];
 const day = process.argv[3];
 const part = process.argv[4];
 const input = process.argv[5];
-const solutionFile = path.resolve(`./${year}/day${day}/part${part}.${EXTENSIONS.javascript}`);
-const inputFile = `./${year}/day${day}/${input}.txt`;
+const solutionFile = path.resolve(`${process.cwd()}/${year}/day${day}/part${part}.mjs`);
+const inputFile = `${process.cwd()}/${year}/day${day}/${input}.txt`;
 
 import(solutionFile, ).then(async ({ solution }) => {
   const input = fs.readFileSync(inputFile, 'utf8');
