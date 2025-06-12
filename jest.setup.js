cc7bd990c2977032bd693b3a11b6d8b9413ff240
Huu Nguyen <nguyenhuuit@yahoo.com>
2025-06-12 21:26:52 +0700

    fix: newline was removed in sample

--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,10 @@
 require('@testing-library/jest-dom');
 
+jest.mock('chalk', () => ({
+  bold: (text) => text,
+  greenBright: (text) => text,
+  yellowBright: (text) => text,
+}));
+
 // Mock console.error to suppress React warnings
 console.error = jest.fn(); 
